60173907a0e8547585e21a36b9cba1889e38f830
laurawarren88 <laura@enterpriseautomation.co.uk>
2024-09-10 12:42:48 +0100

    New DHCP file

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -1,44 +1,51 @@
-#!/bin/bash
+ #!/bin/bash
 #Setting up the DHCP for the network
 
+#Adding google to the path in order to reach the internet
 echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" | tee /etc/resolv.conf
 
+echo "Allow port forwarding"
+sysctl -w net.ipv4.ip_forward=1
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf 2>/dev/null
+
+check_connected=$( nmcli | grep "ens" )
+echo $check_connected
+
+DHCP_IP=$( hostname -I )
+echo "Your hostname is:" $DHCP_IP
+
+#Update mirrorlist and where gets CentOS
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+#Setting up all the variables
 #Add information to network scripts - Boot Protocol, IP Address and Gateway
 NETWORK_SCRIPT="/etc/sysconfig/network-scripts/ifcfg-ens33"
+DHCP_CONFIG="/etc/sysconfig/dhcpd"
 
+# Function to update network config
+update_network_config() {
+    local key=$1
+    local value=$2
+    local file=$3
+
+    if grep -q "^$key=" $file; then
+        sed -i "s/^$key=.*/$key=$value/" $file
+    else
+        echo "$key=$value" >> $file
+    fi
+}
+
+# Set static IP, Gateway, DNS, and domain in network script
+update_network_config "IPADDR" "10.0.0.2" $NETWORK_SCRIPT
+update_network_config "GATEWAY" "10.0.0.1" $NETWORK_SCRIPT
+update_network_config "DNS" "10.0.0.3" $NETWORK_SCRIPT
 # Remove UUID
 sed -i '/^UUID=/d' $NETWORK_SCRIPT
-
-#Adding IP address
-IPADDR=10.0.0.2 >> $NETWORK_SCRIPT
-grep -q '^IPADDR=' $NETWORK_SCRIPT && \
-sed -i 's/^IPADDR=.*/IPADDR=10.0.0.2/' $NETWORK_SCRIPT ||
-echo 'IPADDR=10.0.0.2' >> $NETWORK_SCRIPT
-
-#Adding Gateway address
-GATEWAY=10.0.0.1 >> $NETWORK_SCRIPT
-grep -q '^GATEWAY=' $NETWORK_SCRIPT && \
-sed -i 's/^GATEWAY=.*/GATEWAY=10.0.0.1/' $NETWORK_SCRIPT || \
-echo 'GATEWAY=10.0.0.1' >> $NETWORK_SCRIPT
-
-#echo "Adding DNS1 address"
-DNS1=8.8.8.8 >> $NETWORK_SCRIPT
-grep -q '^DNS1=' $NETWORK_SCRIPT && \
-sed -i 's/^DNS1=.*/DNS1=8.8.8.8/' $NETWORK_SCRIPT || \
-echo 'DNS1=8.8.8.8' >> $NETWORK_SCRIPT
-
-#echo "Adding DNS2 address"
-DNS2=8.8.4.4 >> $NETWORK_SCRIPT
-grep -q '^DNS2=' $NETWORK_SCRIPT && \
-sed -i 's/^DNS2=.*/DNS2=8.8.4.4/' $NETWORK_SCRIPT || \
-echo 'DNS2=8.8.4.4' >> $NETWORK_SCRIPT
-
-#Change the BOOTPROTO to stati
+#Change the BOOTPROTO to static
 sed -i 's/BOOTPROTO=dhcp/BOOTPROTO=static/' $NETWORK_SCRIPT
 
-sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-
+#Ensure the VM connects through the Gateway
 ip route add 172.16.6.0/24 via 10.0.0.1 dev ens33
 ip route add default via 10.0.0.1 dev ens33
 
@@ -49,53 +56,48 @@ setenforce 0
 #Next, setup the firewall
 echo "Now let's setup the Firewall"
 systemctl start firewalld
-#Lets make sure we can reach the internet and allow masquerading
+echo "Lets make sure we can reach the internet and allow masquerading"
 firewall-cmd --add-masquerade --permanent
-#Lets make sure the DHCP port:67 allows traffic and DNS port:53
+echo "Lets make sure the DHCP port:67, 68 & 53 allows traffic"
+echo "Port 67 tcp:"
 firewall-cmd --zone=public --add-port=67/tcp --permanent
+echo “Port 67 udp:
 firewall-cmd --zone=public --add-port=67/udp --permanent
+echo "Port 68 tcp:"
 firewall-cmd --zone=public --add-port=68/tcp --permanent
+echo "Port 68 udp:"
 firewall-cmd --zone=public --add-port=68/udp --permanent
+echo "Port 53 tcp:"
 firewall-cmd --zone=public --add-port=53/tcp --permanent
+echo "Port 53 udp:"
 firewall-cmd --zone=public --add-port=53/udp --permanent
+echo "Allow traffic from other VMs on our network"
 firewall-cmd --permanent --zone=public --add-icmp-block-inversion
+echo "After all those changes lets reload the firewall"
 firewall-cmd --reload
 systemctl restart firewalld
 
-check_connected=$( nmcli | grep "ens" )
-echo $check_connected
-
-DHCP_IP=$( hostname -I )
-echo $DHCP_IP
-
-#Update mirrorlist and where gets CentOS
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-
-#Update packages
-yum update -y
-yum upgrade -y
-
 #Install DHCP
+echo "Installing necessary DHCP packages"
 yum install dhcp-client dhcp-libs dhcp-relay dhcp-server -y
 yum install bind bind-utils -y
 
-#Lets make sure the DHCP listens on the network
-DHCP_CONFIG=/etc/sysconfig/dhcpd
-grep -q '^DHCPDARGS=' $DHCP_CONFIG && \
-sed -i 's/^DHCPDARGS=.*/DHCPDARGS=ens33/' $DHCP_CONFIG || \
-echo 'DHCPDARGS=ens33' >> $DHCP_CONFIG
+#ISetting up other variables now have the packages installed
+NAMED_CONF="/etc/named.conf"
+DHCPD_CONF="/etc/dhcp/dhcpd.conf"
+
+echo "Lets make sure the DHCP listens on the network"
+update_network_config "DHCPDARGS" "ens33" $DHCP_CONFIG
 
 # Generate rndc key and set ownership and permissions
-#echo "Generating rndc key..."
+echo "Generating rndc key..."
 rndc-confgen -a -b 512
 chmod 740 /etc/rndc.key
 echo "Extracting the key"
-#cat /etc/rndc.key
+cat /etc/rndc.key
 # Extract the secret key from the rndc.key file
 RNDC_SECRET=$(grep -Eo 'secret ".*";' /etc/rndc.key | awk '{print $2}' | tr -d '";')
-#echo "Extracted RNDC secret key: $RNDC_SECRET"
-
+echo "Extracted RNDC secret key: $RNDC_SECRET"
 
 # Validate if the RNDC secret is properly extracted
 if [ -z "$RNDC_SECRET" ]; then
@@ -107,31 +109,22 @@ fi
 systemctl restart named
 
 # Modifying named.conf to include controls and zone definitions
-NAMED_CONF="/etc/named.conf"
 if ! grep -q "key \"rndc-key\"" $NAMED_CONF; then
-#   echo "Adding RNDC key to named.conf..."
+    echo "Adding RNDC key to named.conf..."
     cat <<EOL >> $NAMED_CONF
 controls {
     inet 127.0.0.1 allow { localhost; } keys { "rndc-key"; };
 };
-
 key "rndc-key" {
     algorithm hmac-md5;
     secret $RNDC_SECRET;
 };
 EOL
- # Debug: Show confirmation of changes to named.conf
-    echo "named.conf modified:"
-    tail -n 20 $NAMED_CONF
-#else
-#   echo "named.conf already contains controls section."
 fi
 
-# Modify dhcpd.conf to include the secret key for DDNS
-DHCPD_CONF="/etc/dhcp/dhcpd.conf"
-
+# Modify dhcpd.conf to include the secret key for DDNSif ! grep -q "key \"rndc-key\"" $DHCPD_CONF; then
 if ! grep -q "key \"rndc-key\"" $DHCPD_CONF; then
-#    echo "Adding RNDC key to dhcpd.conf..."
+echo "Adding RNDC key to dhcpd.conf..."
     cat <<EOL >> $DHCPD_CONF
 ddns-update-style interim;
 ignore client-updates;
@@ -161,11 +154,6 @@ subnet 10.0.0.0 netmask 255.255.255.0 {
     max-lease-time 7200;
 }
 EOL
-# Debug: Show confirmation of changes to dhcpd.conf
-#    echo "dhcpd.conf modified:"
-#   tail -n 20 $DHCPD_CONF
-else
-    echo "dhcpd.conf already contains key configuration."
 fi
 
 # Restart the DHCP service to apply changes
@@ -173,4 +161,3 @@ systemctl restart dhcpd
 
 #DHCP setup complete
 echo "DHCP complete"
-
